9c1f5435824e829ad9b4d50e1367ac593c20b908
TH429ZC <TH429ZC@IN3261049W1.ey.net>
2022-08-12 13:03:17 +0530

    Added SampleTest.xml file

---
+++ src/test/java/com/learnautomation/testcases/SampleTest.java
@@ -0,0 +1,13 @@
+package com.learnautomation.testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test(description="Added to test generic xml")
+	public void Test1()
+	{
+		System.out.println("Sample Test executed");
+	}
+
+}
